a6bddd0e6f0c7affca5a4fbee846e8ea689ea5c1
OneOfOne <oneofone@gmail.com>
2021-03-30 17:40:17 -0500

    rework the logic for SetWithUpdate and use RWMutex/map

--- stc.go
+++ stc.go
@@ -5,55 +5,98 @@ import (
 	"time"
 )
 
-type SimpleTimedCache struct {
-	m sync.Map
+type entry struct {
+	val      interface{}
+	expireAt int64
 }
 
-func (stc *SimpleTimedCache) Set(key, val interface{}, expiry time.Duration) {
-	stc.m.Store(key, val)
+type SimpleTimedCache struct {
+	m   map[string]*entry
+	mux sync.RWMutex
+}
+
+func (stc *SimpleTimedCache) init() *SimpleTimedCache {
+	if stc.m == nil {
+		stc.m = make(map[string]*entry)
+	}
+	return stc
+}
+
+func (stc *SimpleTimedCache) Set(key string, val interface{}, expiry time.Duration) {
+	expireAt := time.Now().Add(expiry).Unix()
+	stc.mux.Lock()
+	stc.init().m[key] = &entry{val, expireAt}
+	stc.mux.Unlock()
 	if expiry > 0 {
 		time.AfterFunc(expiry, func() {
-			if cv, _ := stc.m.Load(key); cv == val {
-				stc.m.Delete(key)
+			stc.mux.Lock()
+			if e := stc.m[key]; e != nil && e.expireAt == expireAt {
+				delete(stc.m, key)
 			}
+			stc.mux.Unlock()
 		})
 	}
 }
 
-func (stc *SimpleTimedCache) SetWithUpdate(key interface{}, valFn func() interface{}, every time.Duration) {
+func (stc *SimpleTimedCache) SetWithUpdate(key string, valFn func() interface{}, every time.Duration) {
 	val := valFn()
-	stc.m.Store(key, val)
+	stc.mux.Lock()
+	stc.init().m[key] = &entry{val, -1}
+	stc.mux.Unlock()
 	if every == 0 {
 		return
 	}
 
 	var fn func()
 	fn = func() {
-		if !stc.Exists(key) {
+		e := stc.get(key)
+		if e == nil || e.expireAt != -1 {
 			return
 		}
-		stc.m.Store(key, valFn())
-		time.AfterFunc(every, fn)
+		val := valFn()
+		stc.mux.Lock()
+		e = stc.m[key]
+		if e != nil && e.expireAt == -1 {
+			e.val = val
+		}
+		stc.mux.Unlock()
+		if e != nil && e.expireAt == -1 {
+			time.AfterFunc(every, fn)
+		}
 	}
 	time.AfterFunc(every, fn)
 }
 
-func (stc *SimpleTimedCache) Delete(key interface{}) {
-	stc.m.Delete(key)
+func (stc *SimpleTimedCache) Delete(key string) {
+	stc.mux.Lock()
+	delete(stc.m, key)
+	stc.mux.Unlock()
 }
 
-func (stc *SimpleTimedCache) Exists(key interface{}) bool {
-	_, ok := stc.m.Load(key)
+func (stc *SimpleTimedCache) Exists(key string) bool {
+	stc.mux.RLock()
+	ok := stc.m[key] != nil
+	stc.mux.RUnlock()
 	return ok
 }
 
-func (stc *SimpleTimedCache) Get(key interface{}) interface{} {
-	if v, ok := stc.GetOk(key); ok {
-		return v
+func (stc *SimpleTimedCache) Get(key string) interface{} {
+	if e := stc.get(key); e != nil {
+		return e.val
 	}
 	return nil
 }
 
-func (stc *SimpleTimedCache) GetOk(key interface{}) (interface{}, bool) {
-	return stc.m.Load(key)
+func (stc *SimpleTimedCache) GetOk(key string) (interface{}, bool) {
+	if e := stc.get(key); e != nil {
+		return e.val, true
+	}
+	return nil, false
+}
+
+func (stc *SimpleTimedCache) get(key string) *entry {
+	stc.mux.RLock()
+	e := stc.m[key]
+	stc.mux.RUnlock()
+	return e
 }
